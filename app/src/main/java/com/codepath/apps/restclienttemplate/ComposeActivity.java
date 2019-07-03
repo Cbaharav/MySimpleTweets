68aa62e7a47e36affd1d888092a7aefd12c7bdbe
Cbaharav <carmel.baharav@gmail.com>
2019-07-03 15:31:52 -0700

    refresh now works in compose activity when an item is posted and compose activity has new customized action bar

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,13 +1,20 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -25,6 +32,7 @@ public class ComposeActivity extends AppCompatActivity {
     Button btnPost;
     TextView tvChar;
     TwitterClient client;
+    MenuItem miActionProgressItem;
 
     // TextWatcher counts the number of characters, sets tvChar to characters remaining
     private final TextWatcher mTextEditorWatcher = new TextWatcher() {
@@ -54,11 +62,41 @@ public class ComposeActivity extends AppCompatActivity {
         tvChar = (TextView) findViewById(R.id.tvChar);
         // setting textwatcher on etNewTweet
         etNewTweet.addTextChangedListener(mTextEditorWatcher);
+
+        // modifying the action bar, blue background and different title
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ff1da1f2")));
+        bar.setTitle("New Tweet");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_compose, menu);
+
+        // store instance of the menu item containing progress
+        miActionProgressItem = menu.findItem(R.id.miActionProgress);
+        // extract the action-view from the menu item
+        ProgressBar v = (ProgressBar) MenuItemCompat.getActionView(miActionProgressItem);
+        v.setBackgroundColor(getResources().getColor(android.R.color.white));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // toggle visibility of progress bar
+    public void showProgressBar() {
+        // show progress item
+        miActionProgressItem.setVisible(true);
+    }
+
+    public void hideProgressBar() {
+        // hide progress item
+        miActionProgressItem.setVisible(false);
+    }
 
     // post button is clicked on new tweet
     public void onPost(View v) {
+        showProgressBar();
         // instantiating client with this context
         client = TwitterApp.getRestClient(this);
         // call sendTweet from TwitterClient
@@ -74,11 +112,18 @@ public class ComposeActivity extends AppCompatActivity {
                     // set as result of the intent, returning to TimelineActivity
                     setResult(RESULT_OK, i);
                     // close ComposeActivity
+                    hideProgressBar();
                     finish();
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                hideProgressBar();
+                super.onFailure(statusCode, headers, throwable, errorResponse);
             }
         });
 
