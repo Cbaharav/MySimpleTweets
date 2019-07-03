4c1071e6233863f09af8433d9aed3b58a5f5b86d
Cbaharav <carmel.baharav@gmail.com>
2019-07-03 16:33:42 -0700

    profile pictures are now circular

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -1,6 +1,10 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 import java.util.List;
@@ -52,7 +58,47 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         // load user images
         Glide.with(context)
                 .load(tweet.user.profileImageUrl)
+                .transform(new CircleTransform(context))
                 .into(viewHolder.ivProfileImage);
+    }
+
+    // courtesy of http://www.gadgetsaint.com/android/circular-images-glide-library-android/#.XR03f9NKiL8
+    // rounds edges
+    public class CircleTransform extends BitmapTransformation {
+        public CircleTransform(Context context) {
+            super(context);
+        }
+
+        @Override protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+            return circleCrop(pool, toTransform);
+        }
+
+        private Bitmap circleCrop(BitmapPool pool, Bitmap source) {
+            if (source == null) return null;
+
+            int size = Math.min(source.getWidth(), source.getHeight());
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+
+            Bitmap squared = Bitmap.createBitmap(source, x, y, size, size);
+
+            Bitmap result = pool.get(size, size, Bitmap.Config.ARGB_8888);
+            if (result == null) {
+                result = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+            }
+
+            Canvas canvas = new Canvas(result);
+            Paint paint = new Paint();
+            paint.setShader(new BitmapShader(squared, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP));
+            paint.setAntiAlias(true);
+            float r = size / 2f;
+            canvas.drawCircle(r, r, r, paint);
+            return result;
+        }
+
+        @Override public String getId() {
+            return getClass().getName();
+        }
     }
 
     @Override
@@ -67,6 +113,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         public TextView tvBody;
         public TextView tvTime;
         public TextView tvHandle;
+        public ImageView ivReply;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -77,6 +124,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
             tvBody = (TextView) itemView.findViewById(R.id.tvBody);
             tvTime = (TextView) itemView.findViewById(R.id.tvTime);
             tvHandle = (TextView) itemView.findViewById(R.id.tvHandle);
+            ivReply = (ImageView) itemView.findViewById(R.id.ivReply);
         }
     }
 
